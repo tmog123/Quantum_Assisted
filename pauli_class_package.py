a6fc8849851189bbef9b0d3f5c5a00c1f28e0836
tmog123 <41319311+tmog123@users.noreply.github.com>
2021-03-18 16:08:59 +0800

    Update pauli_class_package.py

--- pauli_class_package.py
+++ pauli_class_package.py
@@ -16,40 +16,54 @@ class paulistring(object):
 def pauli_combine(pauli1,pauli2):
     resultN = pauli1.N
     resultcoeff = (pauli1.coefficient)*(pauli2.coefficient)
-    resultstring = ''
+    resultstring = []
     for i in range(pauli1.N):
         subpauli,subcoeff = pauli_helper(pauli1.string[i],pauli2.string[i])
-        resultstring = resultstring + subpauli
+        #resultstring = resultstring + subpauli
+        resultstring.append(subpauli)
         resultcoeff = resultcoeff*subcoeff
     return paulistring(resultN,resultstring,resultcoeff)
 
 
 def pauli_helper(pauli1,pauli2):
-    if pauli1 == 'I':
+    if pauli1 == 0:
         return pauli2,1
-    if pauli2 == 'I':
+    if pauli2 == 0:
         return pauli1,1
-    if pauli1 == 'X':
-        if pauli2 =='X':
-            return 'I',1
-        elif pauli2 =='Y':
-            return 'Z',1j
-        elif pauli2 =='Z':
-            return 'Y',-1j
-    elif pauli1 == 'Y':
-        if pauli2 =='X':
-            return 'Z',-1j
-        elif pauli2 =='Y':
-            return 'I',1
-        elif pauli2 =='Z':
-            return 'X',1j
-    elif pauli1 == 'Z':
-        if pauli2 =='X':
-            return 'Y',1j
-        elif pauli2 =='Y':
-            return 'X',-1j
-        elif pauli2 =='Z':
-            return 'I',1
+    if pauli1 == 1:
+        if pauli2 ==1:
+            return 0,1
+        elif pauli2 ==2:
+            return 3,1j
+        elif pauli2 ==3:
+            return 2,-1j
+    elif pauli1 == 2:
+        if pauli2 ==1:
+            return 3,-1j
+        elif pauli2 ==2:
+            return 0,1
+        elif pauli2 ==3:
+            return 1,1j
+    elif pauli1 == 3:
+        if pauli2 ==1:
+            return 2,1j
+        elif pauli2 ==2:
+            return 1,-1j
+        elif pauli2 ==3:
+            return 0,1
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
