0adb4c1c32facce1c84a4c3937cb1601da972b8d
Lim Kian Hwee <35110651+KianHwee@users.noreply.github.com>
2021-03-22 16:56:54 +0800

    changed numpy matrix to dict in unevaluatedmatrix

--- matrix_class_package.py
+++ matrix_class_package.py
@@ -9,12 +9,13 @@ class unevaluatedmatrix(object):
         self.N = N
         size = len(ansatz.get_moments())
         moments = ansatz.get_moments()
-        numpymatrix = np.zeros([size,size])
+        # numpymatrix = np.zeros([size,size])
+        dictionary_of_matrix_elements = dict()
         if matrixtype == 'E':
             for i in range(size):
                 for j in range(size):
                     element = [pcp.pauli_combine(moments[i].get_paulistring().get_complex_conjugate(),moments[j].get_paulistring())]
-                    numpymatrix[i,j] = element
+                    dictionary_of_matrix_elements[(i,j)] = element
         
         if matrixtype == 'D' or 'O': #actually the O matrix has the same form as the D matrix, just that the observable O takes the place of the Hamiltonian H
             for i in range(size):
@@ -25,17 +26,18 @@ class unevaluatedmatrix(object):
                         a = pcp.pauli_combine(a,moments[j].get_paulistring())
                         element.append(a)
 
-                    numpymatrix[i,j] = element
+                    dictionary_of_matrix_elements[(i,j)] = element
 
-        self.numpymatrix = numpymatrix
-    
+        self.dict_of_uneval_matrix_elems = dictionary_of_matrix_elements
+        self.size = size
+
     def evaluate_matrix_by_matrix_multiplicaton(self, initial_state_object):
         initial_statevector = initial_state_object.get_statevector()
-        size = len(self.numpymatrix[0])
+        size = len(self.size)
         matrix = np.empty([size,size], dtype=np.complex128)
         for i in range(size):
             for j in range(size):
-                paulistrings = self.numpymatrix[(i,j)]
+                paulistrings = self.dict_of_uneval_matrix_elems[(i,j)]
                 #by construction, paulistrings has at least 1 element
                 temp_matrix = paulistrings[0].get_matrixform()
                 for k in range(1, len(paulistrings)):
