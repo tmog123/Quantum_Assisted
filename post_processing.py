bac30eaee87fd28331812a0c2786c948e32cd546
Lim Kian Hwee <35110651+KianHwee@users.noreply.github.com>
2021-04-27 15:37:46 +0800

    No longer need to manually make D and E Hermitian

--- post_processing.py
+++ post_processing.py
@@ -131,10 +131,10 @@ class quantumSimulators(ABC):
         self.has_it_been_evaluated = False
         self.finishedalphas = None
 
-        print('Ensuring E and D matrices are Hermitian by adding cc')
-
-        self.E = (self.E + self.E.conj().T)/2
-        self.D = (self.D + self.D.conj().T)/2
+        #This was required in the past for debugging, but we don't need this now
+        # print('Ensuring E and D matrices are Hermitian by adding cc')
+        # self.E = (self.E + self.E.conj().T)/2
+        # self.D = (self.D + self.D.conj().T)/2
 
     def numberstep(self, steps):
         self.steps = steps 
