c18483787c4453ce546aad2169c65d0008b35e3a
Jonathan Lau <jonathanlau66@gmail.com>
2021-06-01 11:10:53 +0800

    Minor change to cvxpy

--- optimizers.py
+++ optimizers.py
@@ -42,12 +42,12 @@ def cvxpy_density_matrix_routine(D_matrix,E_matrix):
     #D_real = np.real(D_matrix_np)
     #D_imag = np.imag(D_matrix_np)
     #D_realified = np.bmat([[D_real,-D_imag],[D_imag,D_real]])
-    beta = cp.Variable((numstate,numstate),complex=True)
+    beta = cp.Variable((numstate,numstate),hermitian=True)
     #print(np.shape(beta))
 
     # Define and solve the CVXPY problem.
     constraints = [beta >> 0]
-    constraints += [beta == beta.H]
+    #constraints += [beta == beta.H]
     constraints += [cp.trace(E_matrix_np @ beta) == 1]
     prob = cp.Problem(cp.Minimize(cp.real(cp.trace(D_matrix_np @ beta))),constraints)
     prob.solve(solver=cp.MOSEK,verbose=False)
